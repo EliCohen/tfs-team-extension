d3b362dafa4b853f0466c5cc9fe5c3db03d672ee
Eli Cohen <elcohen@varonis.com>
2020-03-03 10:40:11 +0200

    Adding error handle

--- eventPage.js
+++ eventPage.js
@@ -1,20 +1,7 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.type === 'tfsTeamShowIcon') {
-    chrome.pageAction.show(sender.tab.id);
-    chrome.pageAction.setTitle({
-      tabId: sender.tab.id,
-      title: `test`
-    });
-  }
-  if (request.todo == 'showPageAction') {
+  if (message.type == 'showPageAction') {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.pageAction.show(tabs[0].id);
     });
   }
-});
-
-chrome.storage.onChanged.addListener(function(changes, storageName) {
-  chrome.browserAction.setBadgeText({
-    text: changes.summary.newValue.toString()
-  });
 });
--- tfsTeamExtension.js
+++ tfsTeamExtension.js
@@ -1,200 +1,162 @@
-// chrome.runtime.sendMessage({ todo: 'showPageAction' });
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   if (request.todo == 'changeColor') {
-//   }
-// });
+chrome.runtime.sendMessage({ type: 'showPageAction' });
+chrome.runtime.onMessage.addListener(async function(
+  message,
+  sender,
+  sendResponse
+) {
+  if (message.type == 'calculate') {
+    try {
+      let result = await calculateData();
 
-// document.onload = function(event) {
-//   console.log('Ready');
-// };
-
-(function() {
-  const observerTargetElementSelector = '.content-section';
-  const hubContentElementSelector = '.hub-content > div >.rightPane';
-  const discountElementSelector = '.DiscountTag__DiscountText-sc-1nsisq8-1';
-  const priceTagsClassName = '.PriceLabel__Root-tydi84-0>div';
-
-  let sprintDays;
-  let rowsData;
-
-  const mutationsObserver = new MutationObserver((e) => {
-    const hubContentElement = $(hubContentElementSelector);
-    if (!$('#tfsTeamCalculateBtn')[0]) {
-      let panelHeader = $('.backlogs-tool-panel-header', hubContentElement);
-      if (panelHeader[0]) {
-        panelHeader.append(
-          `<span class="tfsTeamHeader">TFS Team Extension:</span>
-        <button id="tfsTeamCalculateBtn">Calculate</button>`
-        );
-
-        $('#tfsTeamCalculateBtn').click(function(event) {
-          calculateData();
-        });
-      }
+      chrome.runtime.sendMessage({
+        type: 'calculateFinish',
+        data: result.data,
+        leftDays: result.leftDays
+      });
+    } catch (e) {
+      chrome.runtime.sendMessage({
+        type: 'calculateError',
+        error: e.message
+      });
     }
+  }
+});
 
-    // if (isThereADiscount() && !isPricesUpdated()) {
-    //   let discount = getDiscount();
-    //   if (typeof discount !== 'undefined') {
-    //     chrome.runtime.sendMessage({
-    //       type: '10bisDiscountShowIcon',
-    //       discount: discount
-    //     });
-    //     updatePrices(discount);
-    //   }
-    // }
-  });
-
-  mutationsObserver.observe(
-    document.querySelector(observerTargetElementSelector),
-    {
-      attributes: false,
-      childList: true,
-      subtree: true
-    }
+async function calculateData() {
+  const capacityPaneElement = $('.capacity-pane-container');
+  const teamProgressElement = $(
+    '.team-capacity-control .progress-text',
+    capacityPaneElement
+  );
+  const teamProgressText = teamProgressElement.text();
+  let sprintDays = Math.floor(
+    parseInt(
+      teamProgressText.slice(
+        teamProgressText.indexOf(' of ') + 4,
+        teamProgressText.indexOf(' h')
+      )
+    ) / 6
   );
 
-  async function calculateData() {
-    rowsData = new Map();
-    const capacityPaneElement = $('.capacity-pane-container');
-    const teamProgressElement = $(
-      '.team-capacity-control .progress-text',
-      capacityPaneElement
-    );
-    const teamProgressText = teamProgressElement.text();
-    sprintDays = sprintDays
-      ? sprintDays
-      : Math.floor(
-          parseInt(
-            teamProgressText.slice(
-              teamProgressText.indexOf(' of ') + 4,
-              teamProgressText.indexOf(' h')
-            )
-          ) / 6
-        );
+  const rowsData = await calculateTeamCommitment();
 
-    const totalCommitmentDays = await calculateTeamCommitment();
+  return {
+    data: Array.from(rowsData.values()),
+    leftDays: sprintDays
+  };
+}
 
-    updateTeamCommitment();
+async function calculateTeamCommitment() {
+  let rowsData = new Map();
+  let currentScroll = $('.productbacklog-grid-results .grid-canvas')[0]
+    .scrollTop;
+  scrollTo(0);
+  await sleep(50);
+  getRowsData(rowsData);
+  scrollToBottom();
+  await sleep(50);
+  getRowsData(rowsData);
+  scrollTo(currentScroll);
 
-    const teamProgressCurrent = $(
-      '.team-capacity-control .visual-progress-current',
-      capacityPaneElement
-    );
+  return rowsData;
+}
 
-    teamProgressCurrent.width(
-      `${Math.max(Math.ceil((totalCommitmentDays / sprintDays) * 100), 100)}%`
-    );
+function getRowsData(rowsData) {
+  const rowsElements = $(
+    '.productbacklog-grid-results .grid-row.grid-row-normal'
+  );
 
-    $('.team-capacity-control .display-text', capacityPaneElement).text(
-      'Team Commitment'
-    );
+  const headerCellsTitles = $(
+    '.productbacklog-grid-results .grid-header .grid-header-column .title'
+  )
+    .map(function() {
+      return $.trim($(this).text());
+    })
+    .get();
 
-    teamProgressElement.text(`(${totalCommitmentDays} of ${sprintDays} Days)`);
+  const orderIndex = headerCellsTitles.indexOf('Order');
+  const titleIndex = headerCellsTitles.indexOf('Title');
+  const stateIndex = headerCellsTitles.indexOf('State');
+  const effortIndex = headerCellsTitles.indexOf('Effort');
+  const assignedToIndex = headerCellsTitles.indexOf('Assigned To');
+
+  validateMandatoryColumns(titleIndex, stateIndex, effortIndex);
+
+  rowsElements.get().forEach((row) => {
+    const orderElement =
+      orderIndex !== -1 ? $('.grid-cell', row)[orderIndex] : 0;
+    const titleElement =
+      titleIndex !== -1
+        ? $('.work-item-type-icon', $('.grid-cell', row)[titleIndex])
+        : undefined;
+    const stateElement =
+      stateIndex !== -1
+        ? $('.workitem-state-value', $('.grid-cell', row)[stateIndex])
+        : undefined;
+    const effortElement =
+      effortIndex !== -1 ? $('.grid-cell', row)[effortIndex] : 0;
+    const assignedToElement =
+      assignedToIndex !== -1
+        ? $(
+            '.identity-view-control span',
+            $('.grid-cell', row)[assignedToIndex]
+          )
+        : undefined;
+
+    const orderValue = orderElement ? orderElement.innerText : '0';
+    if (!rowsData.has(orderValue))
+      rowsData.set(orderValue, {
+        type: titleElement ? titleElement.attr('aria-label') : '',
+        state: stateElement ? stateElement.text() : '',
+        effort:
+          effortElement && isInt(effortElement.innerText)
+            ? parseInt(effortElement.innerText)
+            : 0,
+        assignedTo: assignedToElement ? assignedToElement.text() : ''
+      });
+  });
+}
+
+function validateMandatoryColumns(titleIndex, stateIndex, effortIndex) {
+  let missingColumns = [];
+
+  if (titleIndex === -1) {
+    missingColumns.push('Title');
+  }
+  if (stateIndex === -1) {
+    missingColumns.push('State');
+  }
+  if (effortIndex === -1) {
+    missingColumns.push('Effort');
   }
 
-  async function calculateTeamCommitment() {
-    let currentScroll = $('.productbacklog-grid-results .grid-canvas')[0]
-      .scrollTop;
-    scrollTo(0);
-    await sleep(50);
-    getRowsData();
-    scrollToBottom();
-    await sleep(50);
-    getRowsData();
-    scrollTo(currentScroll);
-
-    let teamTotalCommitment = 0;
-    rowsData.forEach((value, key) => {
-      if (isPbi(value)) {
-        teamTotalCommitment += !['New', 'Approved'].includes(value.state)
-          ? value.effort
-          : 0;
-      }
-    });
-
-    return teamTotalCommitment;
+  if (missingColumns.length > 0) {
+    let missingMessage =
+      missingColumns.length === 1
+        ? ' column is missing! Please add it and try again.'
+        : ' columns are missing! Please add them and try again.';
+    throw new Error(missingColumns.join(', ') + missingMessage);
   }
+}
 
-  function getRowsData() {
-    const rowsElements = $(
-      '.productbacklog-grid-results .grid-row.grid-row-normal'
-    );
+function isInt(value) {
+  return (
+    !isNaN(value) &&
+    parseInt(Number(value)) == value &&
+    !isNaN(parseInt(value, 10))
+  );
+}
 
-    const headerCellsTitles = $(
-      '.productbacklog-grid-results .grid-header .grid-header-column .title'
-    )
-      .map(function() {
-        return $.trim($(this).text());
-      })
-      .get();
+function scrollTo(value) {
+  let gridElement = $('.productbacklog-grid-results .grid-canvas')[0];
+  gridElement.scrollTo(0, value);
+}
 
-    const orderIndex = headerCellsTitles.indexOf('Order');
-    const titleIndex = headerCellsTitles.indexOf('Title');
-    const stateIndex = headerCellsTitles.indexOf('State');
-    const effortIndex = headerCellsTitles.indexOf('Effort');
-    const assignedToIndex = headerCellsTitles.indexOf('Assigned To');
+function scrollToBottom() {
+  let gridElement = $('.productbacklog-grid-results .grid-canvas')[0];
+  scrollTo(gridElement.scrollHeight);
+}
 
-    rowsElements.get().forEach((row) => {
-      const orderElement =
-        orderIndex !== -1 ? $('.grid-cell', row)[orderIndex] : 0;
-      const titleElement =
-        titleIndex !== -1
-          ? $('.work-item-type-icon', $('.grid-cell', row)[titleIndex])
-          : undefined;
-      const stateElement =
-        stateIndex !== -1
-          ? $('.workitem-state-value', $('.grid-cell', row)[stateIndex])
-          : undefined;
-      const effortElement =
-        effortIndex !== -1 ? $('.grid-cell', row)[effortIndex] : 0;
-      const assignedToElement =
-        assignedToIndex !== -1
-          ? $(
-              '.identity-view-control span',
-              $('.grid-cell', row)[assignedToIndex]
-            )
-          : undefined;
-
-      const orderValue = orderElement ? orderElement.innerText : '0';
-      if (!rowsData.has(orderValue))
-        rowsData.set(orderValue, {
-          type: titleElement ? titleElement.attr('aria-label') : '',
-          state: stateElement ? stateElement.text() : '',
-          effort:
-            effortElement && isInt(effortElement.innerText)
-              ? parseInt(effortElement.innerText)
-              : 0,
-          assignedTo: assignedToElement ? assignedToElement.text() : ''
-        });
-    });
-  }
-
-  function isInt(value) {
-    return (
-      !isNaN(value) &&
-      parseInt(Number(value)) == value &&
-      !isNaN(parseInt(value, 10))
-    );
-  }
-
-  function isPbi(rowData) {
-    return rowData.type === 'Product Backlog Item';
-  }
-
-  function updateTeamCommitment() {}
-
-  function scrollTo(value) {
-    let gridElement = $('.productbacklog-grid-results .grid-canvas')[0];
-    gridElement.scrollTo(0, value);
-  }
-
-  function scrollToBottom() {
-    let gridElement = $('.productbacklog-grid-results .grid-canvas')[0];
-    scrollTo(gridElement.scrollHeight);
-  }
-
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-})();
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
